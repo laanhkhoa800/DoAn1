b2fa3a67737b15388562bfa26f23ae224be321e5
quocdai03032000 <quocdai03032000@gmail.com>
2021-03-24 09:09:01 +0700

    add database vao project + create sach

---
+++ DoAn1_QuanLyThuVien/DoAn1_QuanLyThuVien/Controllers/SachController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DoAn1_QuanLyThuVien.Models;
+
+namespace DoAn1_QuanLyThuVien.Controllers
+{
+    public class SachController : Controller
+    {
+        QuanLyThuVienEntities database = new QuanLyThuVienEntities();
+        // GET: Sach
+        public ActionResult Index()
+        {
+            return View(database.Saches.ToList());
+        }
+
+        //Tạo mới sách
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Sach sach)
+        {
+            database.Saches.Add(sach);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //Xem Chi tiết sách        
+        public ActionResult Details(int id)
+        {
+            return View(database.Saches.Where(a => a.MaSach == id).FirstOrDefault());
+           
+            
+        }
+    }
+}
